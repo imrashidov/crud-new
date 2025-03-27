1b3cc3b1d61d77360f49d74fe00adb48c6552400
imrashidov <farikrashidov@gmail.com>
2025-03-27 15:23:11 +0400

    Add: DTO for auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
-@Injectable({})
+@Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
   signup() {
