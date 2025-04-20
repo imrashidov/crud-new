f935d7252ae3bfce99dc571ed6da251801e45b38
Farid Rashidov <farikrashidov@gmail.com>
2025-04-20 16:32:30 +0400

    Add: Homepage response

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from "@nestjs/common";
 import { AuthModule } from "./auth/auth.module";
 import { UserModule } from "./user/user.module";
 import { BookmarkModule } from "./bookmark/bookmark.module";
-import { PrismaModule } from './prisma/prisma.module';
+import { PrismaModule } from "./prisma/prisma.module";
+import { AppController } from "./app.controller";
 
 @Module({
   imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
+  controllers: [AppController],
 })
 export class AppModule {}
